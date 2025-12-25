4ad61a81a969a4fecf3e97903a562a68d4b80681
うそかみ <naksriphomtanasate@gmail.com>
2025-12-25 21:31:13 +0700

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def ny(ctx):
             )
             break
 
-        sleep_time = 10 if total > 600 else 1
+        sleep_time = 8 if total > 120 else 1
 
         d = total // 86400
         h = (total % 86400) // 3600
